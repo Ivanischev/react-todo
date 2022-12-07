3efb83a8915e69d5964b0a4fbd34556a567b5d04
Ivan Ivanischev <ivanischev@MacBook-Pro-Axbx.local>
2022-12-07 18:45:23 +0200

    added and-design/icons. Added form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <div className="app-body">
         <h4>Todo List</h4>
+        <Form />
       </div>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import {PlusOutlined} from "@ant-design/icons"
 
 const Form = () => {
     return (
-        <h5>Form</h5>
+        <form>
+            <h3>Form</h3>
+            <input type="text" className='todo-input'/>
+            <button className='todo-button' type='submit'>
+                <PlusOutlined />
+            </button>
+            <div className='select'>
+                <select name="todos" className='filter-todo'>
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
     );
 }
+
+export default Form;
